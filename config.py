1e2efa256e0b4b5305cebf64edd4e4e4cb56e288
Radhe-Radheformyway <cjangbahadur986@gmail.com>
2025-01-07 22:34:17 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY 
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7870406536:AAETVO3PwfLihzOdGFMiOdiKm8JgHQy1-Vs")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("@YAMRAJ_ON_TOP")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("5786411100")
+
+
+
+
+
+
+
+  
